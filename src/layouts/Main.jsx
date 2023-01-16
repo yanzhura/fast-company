01312e9442524a528fd8ad39551e2897a804fda8
Mikhail Yanzhura <yanzhura@gmail.com>
2023-01-16 13:51:50 +0300

    Подготовка к деплою на github pages

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,23 +1,9 @@
 import React from 'react';
-import useMockData from '../utils/mockData';
 
 const Main = () => {
-    const { error, initialize, progress, status } = useMockData();
-    const handleClick = () => {
-        initialize();
-    };
     return (
         <div className="container mt-5">
             <h1 className="m-2">Главная страница</h1>
-            <h3>Инициализация данных в FireBase</h3>
-            <ul>
-                <li>Status: {status}</li>
-                <li>Progress: {progress}</li>
-                {error && <li>Error: {error}</li>}
-            </ul>
-            <button className="btn btn-primary" onClick={handleClick}>
-                Инициализировать
-            </button>
         </div>
     );
 };
